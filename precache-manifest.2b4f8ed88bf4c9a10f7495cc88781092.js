5c10fa256300b09a3e9115210ebc50d0a5b80822
rosgeorg <js.2701e@gmail.com>
2020-09-13 12:59:23 -0500

    Updates

---
+++ precache-manifest.2b4f8ed88bf4c9a10f7495cc88781092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908f5bc4302e377939c9ae4b8f3e9d90",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6722e427b811dba169fe",
+    "url": "/my-portfolio/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/my-portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6722e427b811dba169fe",
+    "url": "/my-portfolio/static/js/main.16f2a1b2.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  }
+]);
